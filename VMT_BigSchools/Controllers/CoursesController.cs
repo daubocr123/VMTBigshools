16315c2a0dd4c5c96278454c90cd06df7207b113
TOAN CAU COMPUTER <TOAN CAU COMPUTER@TOANCAUCOMPUTER>
2019-04-05 16:39:10 +0700

    16:38pm 4/5/2019

--- VMT_BigSchools/Controllers/CoursesController.cs
+++ VMT_BigSchools/Controllers/CoursesController.cs
@@ -69,5 +69,17 @@ namespace VMT_BigSchools.Controllers
 
             return View(viewModel);
         }
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var courses = _dbContext.Courses
+                .Where(c => c.LecturerId == userId && c.DateTime > DateTime.Now)
+                .Include(l => l.Lecturer)
+                .Include(c => c.Category)
+                .ToList();
+
+            return View(courses);
+        }
     }
 }
